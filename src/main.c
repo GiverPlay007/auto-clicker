7481afe552e5534641f9a83747dfd6d0031fcb15
GiverPlay <play.giver.007@gmail.com>
2021-06-14 15:19:57 -0300

    Abstract click to a function

--- src/main.c
+++ src/main.c
@@ -2,13 +2,21 @@
 #include <stdio.h>
 #include <libevdev/libevdev-uinput.h>
 
+void click(struct libevdev_uinput *dev)
+{
+  libevdev_uinput_write_event(dev, EV_KEY, BTN_LEFT, 1);
+  libevdev_uinput_write_event(dev, EV_SYN, SYN_REPORT, 0);
+  libevdev_uinput_write_event(dev, EV_KEY, BTN_LEFT, 0);
+  libevdev_uinput_write_event(dev, EV_SYN, SYN_REPORT, 0);
+}
+
 int main()
 {
   struct libevdev *dev;
   struct libevdev_uinput *uidev;
 
   dev = libevdev_new();
-  libevdev_set_name(dev, "Fake device");
+  libevdev_set_name(dev, "Clicker");
 
   libevdev_enable_event_type(dev, EV_KEY);
   libevdev_enable_event_code(dev, EV_KEY, BTN_LEFT, NULL);
@@ -16,14 +24,15 @@ int main()
   int err = libevdev_uinput_create_from_device(dev, LIBEVDEV_UINPUT_OPEN_MANAGED, &uidev);
 
   if (err != 0)
-    return err;
-
-  for(int i = 0; i < 10; i++)
   {
-    libevdev_uinput_write_event(uidev, EV_KEY, BTN_LEFT, 1);
-    libevdev_uinput_write_event(uidev, EV_SYN, SYN_REPORT, 0);
-    libevdev_uinput_write_event(uidev, EV_KEY, BTN_LEFT, 0);
-    libevdev_uinput_write_event(uidev, EV_SYN, SYN_REPORT, 0);
+    printf("Error: %i\n", err);
+    return err;
+  }
+
+  while(1)
+  {
+    system("sleep 0.25");
+    click(uidev);
   }
 
   libevdev_uinput_destroy(uidev);
